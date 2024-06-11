b7bb7c9b55bcdabfdb29b005bb365f66e9762caf
Aashma lamichhane <aashma.lamichhane@tekvortex.com>
2024-06-11 12:12:36 +0545

    Wait Methods Practice

---
+++ seleniumwebdriver/src/test/java/st3webmethods/WaitMethods.java
@@ -0,0 +1,45 @@
+package st3webmethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitMethods {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+		//Implicit waits
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		driver.manage().window().maximize();
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("Admin");
+
+
+		//Explicit waits
+		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10)); //declaration
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+
+		WebElement usernametext = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder=\"Username\"]")));
+		usernametext.sendKeys("Admin");
+
+		WebElement password = driver.findElement(By.xpath("//input[@placeholder=\"Password\"]"));
+		password.sendKeys("admin123");
+
+		WebElement loginbtn = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()=\"Login\"]")));
+		loginbtn.click();
+
+
+
+
+	}
+
+}
